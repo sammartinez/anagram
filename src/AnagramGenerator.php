ccaadb3baa82ca69d73269416a7e34d32ab8fbd5
Sam Martinez & Ben Casalino <student@epicodus.com>
2015-08-11 12:14:33 -0700

    This commit is saving work going throughthe project

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -6,16 +6,16 @@
         function makeAnagramCase($input_title)
 
         {
-            $library_words = array("ate");
+            //$error_message = "Not in the list of words";
+            $library_words = array("eat", "ate", "tea" );
             $input_array_of_words = explode(" ", $input_title);
             $output_anagramed = array();
 
             foreach ($input_array_of_words as $word) {
+                array_push($output_anagramed, $word);
 
-                if($input_array_of_words == $library_words) {
-                    array_push($output_anagramed, $word);
-                }
             }
+
             return implode(" ", $output_anagramed);
         }
     }
